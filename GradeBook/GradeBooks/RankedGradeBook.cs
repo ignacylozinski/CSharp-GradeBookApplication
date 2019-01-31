caf077bca966cc031b8095b2abdd2f6056387aa7
Ignacy <ignacy.lozinski@norconsult.com>
2019-01-31 14:57:19 +0100

    next commit

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -30,5 +30,23 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count > 5)
+                base.CalculateStatistics();
+            else
+                Console.WriteLine("Ranked grading requires at least 5 students with grades" +
+                                  " in order to properly calculate a student's overall grade.");
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count > 5)
+                base.CalculateStudentStatistics(name);
+            else
+                Console.WriteLine("Ranked grading requires at least 5 students with grades" +
+                                  " in order to properly calculate a student's overall grade.");
+        }
     }
 }
