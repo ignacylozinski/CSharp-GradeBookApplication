1a4c90dcdca4701719eb6ea3c22122d704aa1d09
Ignacy <ignacy.lozinski@norconsult.com>
2019-01-31 09:42:51 +0100

    task 3 override method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,28 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked-grading" +
+                                                    " requires a minimum of 5 students to work");
+            var studentGroup = (int)Math.Ceiling(Students.Count * 0.2);
+
+            var grades = Students.OrderByDescending(g => g.AverageGrade).Select(g => g.AverageGrade).ToList();
+            if (grades[studentGroup - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(studentGroup * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(studentGroup * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(studentGroup * 4) - 1] <= averageGrade)
+                return 'D';
+            else if (grades[(studentGroup * 5) - 1] <= averageGrade)
+                return 'E';
+            else
+                return 'F';
         }
     }
 }
